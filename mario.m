6d1a69aa375fa5944f0212332b5f115e63ed263d
CharlieKC <charlie.kruczko@gmail.com>
2018-04-13 12:16:38 +0100

    added the mario theme function

---
+++ mario.m
@@ -0,0 +1,353 @@
+function mario(nxt)
+    nxt.beep(660,100);
+    pause(0.150);
+    nxt.beep(660,100);
+    pause(0.300);
+    nxt.beep(660,100);
+    pause(0.300);
+    nxt.beep(510,100);
+    pause(0.100);
+    nxt.beep(660,100);
+    pause(0.300);
+    nxt.beep(770,100);
+    pause(0.550);
+    nxt.beep(380,100);
+    pause(0.575);
+
+    nxt.beep(510,100);
+    pause(0.450);
+    nxt.beep(380,100);
+    pause(0.400);
+    nxt.beep(320,100);
+    pause(0.500);
+    nxt.beep(440,100);
+    pause(0.300);
+    nxt.beep(480,80);
+    pause(0.330);
+    nxt.beep(450,100);
+    pause(0.150);
+    nxt.beep(430,100);
+    pause(0.300);
+    nxt.beep(380,100);
+    pause(0.200);
+    nxt.beep(660,80);
+    pause(0.200);
+    nxt.beep(760,50);
+    pause(0.150);
+    nxt.beep(860,100);
+    pause(0.300);
+    nxt.beep(700,80);
+    pause(0.150);
+    nxt.beep(760,50);
+    pause(0.350);
+    nxt.beep(660,80);
+    pause(0.300);
+    nxt.beep(520,80);
+    pause(0.150);
+    nxt.beep(580,80);
+    pause(0.150);
+    nxt.beep(480,80);
+    pause(0.500);
+
+    nxt.beep(510,100);
+    pause(0.450);
+    nxt.beep(380,100);
+    pause(0.400);
+    nxt.beep(320,100);
+    pause(0.500);
+    nxt.beep(440,100);
+    pause(0.300);
+    nxt.beep(480,80);
+    pause(0.330);
+    nxt.beep(450,100);
+    pause(0.150);
+    nxt.beep(430,100);
+    pause(0.300);
+    nxt.beep(380,100);
+    pause(0.200);
+    nxt.beep(660,80);
+    pause(0.200);
+    nxt.beep(760,50);
+    pause(0.150);
+    nxt.beep(860,100);
+    pause(0.300);
+    nxt.beep(700,80);
+    pause(0.150);
+    nxt.beep(760,50);
+    pause(0.350);
+    nxt.beep(660,80);
+    pause(0.300);
+    nxt.beep(520,80);
+    pause(0.150);
+    nxt.beep(580,80);
+    pause(0.150);
+    nxt.beep(480,80);
+    pause(0.500);
+
+    nxt.beep(500,100);
+    pause(0.300);
+
+    nxt.beep(760,100);
+    pause(0.100);
+    nxt.beep(720,100);
+    pause(0.150);
+    nxt.beep(680,100);
+    pause(0.150);
+    nxt.beep(620,150);
+    pause(0.300);
+
+    nxt.beep(650,150);
+    pause(0.300);
+    nxt.beep(380,100);
+    pause(0.150);
+    nxt.beep(430,100);
+    pause(0.150);
+
+    nxt.beep(500,100);
+    pause(0.300);
+    nxt.beep(430,100);
+    pause(0.150);
+    nxt.beep(500,100);
+    pause(0.100);
+    nxt.beep(570,100);
+    pause(0.220);
+
+    nxt.beep(500,100);
+    pause(0.300);
+
+    nxt.beep(760,100);
+    pause(0.100);
+    nxt.beep(720,100);
+    pause(0.150);
+    nxt.beep(680,100);
+    pause(0.150);
+    nxt.beep(620,150);
+    pause(0.300);
+
+    nxt.beep(650,200);
+    pause(0.300);
+
+    nxt.beep(1020,80);
+    pause(0.300);
+    nxt.beep(1020,80);
+    pause(0.150);
+    nxt.beep(1020,80);
+    pause(0.300);
+
+    nxt.beep(380,100);
+    pause(0.300);
+    nxt.beep(500,100);
+    pause(0.300);
+
+    nxt.beep(760,100);
+    pause(0.100);
+    nxt.beep(720,100);
+    pause(0.150);
+    nxt.beep(680,100);
+    pause(0.150);
+    nxt.beep(620,150);
+    pause(0.300);
+
+    nxt.beep(650,150);
+    pause(0.300);
+    nxt.beep(380,100);
+    pause(0.150);
+    nxt.beep(430,100);
+    pause(0.150);
+
+    nxt.beep(500,100);
+    pause(0.300);
+    nxt.beep(430,100);
+    pause(0.150);
+    nxt.beep(500,100);
+    pause(0.100);
+    nxt.beep(570,100);
+    pause(0.420);
+
+    nxt.beep(585,100);
+    pause(0.450);
+
+    nxt.beep(550,100);
+    pause(0.420);
+
+    nxt.beep(500,100);
+    pause(0.360);
+
+    nxt.beep(380,100);
+    pause(0.300);
+    nxt.beep(500,100);
+    pause(0.300);
+    nxt.beep(500,100);
+    pause(0.150);
+    nxt.beep(500,100);
+    pause(0.300);
+
+    nxt.beep(500,100);
+    pause(0.300);
+
+    nxt.beep(760,100);
+    pause(0.100);
+    nxt.beep(720,100);
+    pause(0.150);
+    nxt.beep(680,100);
+    pause(0.150);
+    nxt.beep(620,150);
+    pause(0.300);
+
+    nxt.beep(650,150);
+    pause(0.300);
+    nxt.beep(380,100);
+    pause(0.150);
+    nxt.beep(430,100);
+    pause(0.150);
+
+    nxt.beep(500,100);
+    pause(0.300);
+    nxt.beep(430,100);
+    pause(0.150);
+    nxt.beep(500,100);
+    pause(0.100);
+    nxt.beep(570,100);
+    pause(0.220);
+
+    nxt.beep(500,100);
+    pause(0.300);
+
+    nxt.beep(760,100);
+    pause(0.100);
+    nxt.beep(720,100);
+    pause(0.150);
+    nxt.beep(680,100);
+    pause(0.150);
+    nxt.beep(620,150);
+    pause(0.300);
+
+    nxt.beep(650,200);
+    pause(0.300);
+
+    nxt.beep(1020,80);
+    pause(0.300);
+    nxt.beep(1020,80);
+    pause(0.150);
+    nxt.beep(1020,80);
+    pause(0.300);
+
+    nxt.beep(380,100);
+    pause(0.300);
+    nxt.beep(500,100);
+    pause(0.300);
+
+    nxt.beep(760,100);
+    pause(0.100);
+    nxt.beep(720,100);
+    pause(0.150);
+    nxt.beep(680,100);
+    pause(0.150);
+    nxt.beep(620,150);
+    pause(0.300);
+
+    nxt.beep(650,150);
+    pause(0.300);
+    nxt.beep(380,100);
+    pause(0.150);
+    nxt.beep(430,100);
+    pause(0.150);
+
+    nxt.beep(500,100);
+    pause(0.300);
+    nxt.beep(430,100);
+    pause(0.150);
+    nxt.beep(500,100);
+    pause(0.100);
+    nxt.beep(570,100);
+    pause(0.420);
+
+    nxt.beep(585,100);
+    pause(0.450);
+
+    nxt.beep(550,100);
+    pause(0.420);
+
+    nxt.beep(500,100);
+    pause(0.360);
+
+    nxt.beep(380,100);
+    pause(0.300);
+    nxt.beep(500,100);
+    pause(0.300);
+    nxt.beep(500,100);
+    pause(0.150);
+    nxt.beep(500,100);
+    pause(0.300);
+
+    nxt.beep(500,60);
+    pause(0.150);
+    nxt.beep(500,80);
+    pause(0.300);
+    nxt.beep(500,60);
+    pause(0.350);
+    nxt.beep(500,80);
+    pause(0.150);
+    nxt.beep(580,80);
+    pause(0.350);
+    nxt.beep(660,80);
+    pause(0.150);
+    nxt.beep(500,80);
+    pause(0.300);
+    nxt.beep(430,80);
+    pause(0.150);
+    nxt.beep(380,80);
+    pause(0.600);
+
+    nxt.beep(500,60);
+    pause(0.150);
+    nxt.beep(500,80);
+    pause(0.300);
+    nxt.beep(500,60);
+    pause(0.350);
+    nxt.beep(500,80);
+    pause(0.150);
+    nxt.beep(580,80);
+    pause(0.150);
+    nxt.beep(660,80);
+    pause(0.550);
+
+    nxt.beep(870,80);
+    pause(0.325);
+    nxt.beep(760,80);
+    pause(0.600);
+
+    nxt.beep(500,60);
+    pause(0.150);
+    nxt.beep(500,80);
+    pause(0.300);
+    nxt.beep(500,60);
+    pause(0.350);
+    nxt.beep(500,80);
+    pause(0.150);
+    nxt.beep(580,80);
+    pause(0.350);
+    nxt.beep(660,80);
+    pause(0.150);
+    nxt.beep(500,80);
+    pause(0.300);
+    nxt.beep(430,80);
+    pause(0.150);
+    nxt.beep(380,80);
+    pause(0.600);
+
+    nxt.beep(660,100);
+    pause(0.150);
+    nxt.beep(660,100);
+    pause(0.300);
+    nxt.beep(660,100);
+    pause(0.300);
+    nxt.beep(510,100);
+    pause(0.100);
+    nxt.beep(660,100);
+    pause(0.300);
+    nxt.beep(770,100);
+    pause(0.550);
+    nxt.beep(380,100);
+    pause(0.575);
+end
--- test.m
+++ test.m
@@ -1,6 +1,7 @@
-clf('reset');
+clf('reset'); %resets figures 
 clc;        %clears console
 clear all;      %clears workspace
+close all; %clears figures
 
 nxt = Robot(); %creates robot object
 nxt.beep(440, 200); %Beep beep
@@ -8,16 +9,30 @@ nxt.beep(440, 200); %Beep beep
 %try block means the robot will always be able to exit using nxt.close() even if there is an
 %error in the code
 try
-    nxt.setUpScanner();
-    nxt.sensorAngle()
-    numScans = 72;
-    scanA = nxt.rotScan(numScans)
-    scanB = nxt.rotScan(numScans)
-    polarplot(linspace(0,2*pi,numScans),scanA(:,1), '-*')
-    hold on
-    polarplot(linspace(0,2*pi,numScans),scanB(:,1), '-*')
-    legend('scan1', 'scan2');
-    hold off
+%     nxt.sensorAngle()
+%     numScans = 40;
+%     scanA = nxt.rotScan(numScans);
+%     pause(0.3)
+%     scanB = nxt.rotScan(numScans);  
+%     pause(0.3)
+%     scanC = nxt.rotScan(numScans);
+%     pause(0.3)
+%     scanD = nxt.rotScan(numScans);
+%     figure
+%     polarplot(linspace(0,2*pi,numScans),scanA(:,1), '-*')
+%     hold on
+%     polarplot(linspace(0,2*pi,numScans),scanB(:,1), '-*')
+%     legend('scan1', 'scan2');
+%     hold off
+%     figure
+%     plot(1:numScans, scanA);
+%     hold on
+%     plot(1:numScans, scanB);
+%     plot(1:numScans, scanC);
+%     plot(1:numScans, scanD);
+%     hold off
+%     legend('scan1', 'scan2', 'scan3', 'scan4');
+    mario(nxt);
 catch ME
     warning('There was an error. Closing Robot Connection')
     display(ME.message) %print the error message
