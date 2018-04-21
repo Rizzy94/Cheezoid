698d441073f79799c053bfafb6fc09afec942e94
smcc <smcc@users.noreply.github.com>
2018-04-21 14:00:41 +0200

    pathFollow now runs a path without checking, PlanAndFollow is updated to run with new pathFollow

--- pathFollow.m
+++ pathFollow.m
@@ -1,6 +1,6 @@
 function [nxt,arrived,lost] = pathFollow(nxt, to)
     map = [0,0;60,0;60,45;45,45;45,59;106,59;106,105;0,105]; %default map
-
+    lost = 0;
     from = nxt.pos;
     %work out the distance and angle between from and to
     xDif = to(1)-from(1);
